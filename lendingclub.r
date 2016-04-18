747795b1d9f998a94f8eb9338b0050093e3e44d2
Anil Shrestha <anilshrestha@outlook.com>
2016-04-19 02:05:36 +0545

    Update lendingclub.r

--- lendingclub.r
+++ lendingclub.r
@@ -1,3 +1,7 @@
+## The project is based on the Lending Club's Loan Data for the Year of 2015. 
+## The project intends to provide an insight on Loan's Performance based on the 
+## various variables that were captured by the Lending Club like "Loan Amount","Borrower's Grade", "Employment Grade" etc. 
+
 # library packages
 library(aod) # Analysis
 library(dplyr) # data manipulation 
